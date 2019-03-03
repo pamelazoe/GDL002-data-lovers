8045511ef730bb21b4e8d4eb321b23b525b162d4
Pamela Zoé <zoe@tests-MacBook-Pro.local>
2019-03-03 11:26:21 -0600

    Se agregó función de filtrado, pasa los test

--- src/data.js
+++ src/data.js
@@ -6,32 +6,29 @@
 // };
 
 // window.example = example;
-// const POKEMON = () =>{
-//   return POKEMON;
+
 //};
 
-window.pokeLovers = {
+window.dataLovers = {
   pokeFilter: (pokeData, pokeType) => {
-    //console.log(pokeType)
-    let filterByType = pokeData.filter(pokeData => pokeData.type.includes(pokeType));
-    return filterByType;
+      //console.log(pokeType)
+      const filterByType = pokeData.filter(pokeData => pokeData.type.includes(pokeType));
+      return filterByType;
   },
 
-
-  sort: (pokeData, sortBy, sortOrder) =>{
-    if (sortBy == "name" && sortOrder == "A-Z") {
-      return pokeData.sort((a, b) => (a.name) - (b.name));
-    } else if (sortBy == "name" && sortOrder == "Z-A") {
-      return pokeData.sort((a, b) => (b.name)- (a.name));
-    } else if (sortBy == "number" && sortOrder == "001-151") {
-      return pokeData.sort((a, b) => parseFloat(a.number) - parseFloat(b.number));
-    } else if (sortBy == "number" && sortOrder == "151-001") {
-      return pokeData.sort((a, b) => parseFloat(b.number) - parseFloat(a.number));
-    } else {
-      return pokeData;
+  pokeSort: (pokeData, sortBy) =>{
+    if (sortBy == "a-Z"){
+      return pokeData.sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0));
     }
-
+    if (sortBy == "Z-a"){
+      return pokeData.sort((a, b) => (a.name > b.name ? -1 : b.name > a.name ? 1 : 0));
+    }
+    if (sortBy == "001-151"){
+      return pokeData.sort((a, b) => (a.num > b.num ? 1 : b.num > a.num ? -1 : 0));
+    }
+    if (sortBy == "151-001"){
+      return pokeData.sort((a, b) => (a.num > b.num ? -1 : b.num > a.num ? 1 : 0));
+    }
   }
 
-}
-
+};
