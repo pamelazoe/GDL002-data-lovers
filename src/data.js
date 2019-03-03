b8b0bf1b9e7b72f4644d019e54977c9916ab7a4a
Pamela Zoé <zoe@tests-MacBook-Pro.local>
2019-03-03 11:58:01 -0600

    Agregando cambios a master

--- src/data.js
+++ src/data.js
@@ -1,8 +1,34 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-};
+// const example = () => {
+//   return 'example';
+// };
 
-window.example = example;
+// window.example = example;
+
+//};
+
+window.dataLovers = {
+  pokeFilter: (pokeData, pokeType) => {
+      //console.log(pokeType)
+      const filterByType = pokeData.filter(pokeData => pokeData.type.includes(pokeType));
+      return filterByType;
+  },
+
+  pokeSort: (pokeData, sortBy) =>{
+    if (sortBy == "a-Z"){
+      return pokeData.sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0));
+    }
+    if (sortBy == "Z-a"){
+      return pokeData.sort((a, b) => (a.name > b.name ? -1 : b.name > a.name ? 1 : 0));
+    }
+    if (sortBy == "001-151"){
+      return pokeData.sort((a, b) => (a.num > b.num ? 1 : b.num > a.num ? -1 : 0));
+    }
+    if (sortBy == "151-001"){
+      return pokeData.sort((a, b) => (a.num > b.num ? -1 : b.num > a.num ? 1 : 0));
+    }
+  }
+
+};
