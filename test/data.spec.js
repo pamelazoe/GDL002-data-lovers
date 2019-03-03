f0c001480f64fab6537e05db20edf3095c76ad17
Pamela Zoé <zoe@tests-MacBook-Pro.local>
2019-03-03 11:33:47 -0600

    Se agregó test para función de filtrado

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,12 +1,29 @@
 require('../src/data.js');
+const pokemon = require('../src/data/pokemon/pokemon.json');
+//const filterByTypeMock = require('../src/data/pokemon/filterByTypeMock.json');
+// const sortDataMock  = require('../src/data/pokemon/sortDataMck.json');
+// const computeStatsMock = require('../src/data/pokemon/computeStatsMock.json');
 
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
-  });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+describe('pokemon',()=>{
+  it('is an object',()=>{
+    expect(typeof pokemon).toBe('object');
   });
 });
+
+describe('dataLovers',()=>{
+  it('is an object',()=>{
+    expect(typeof dataLovers).toBe('object');
+  });
+});
+
+describe('pokeFilter()',()=>{
+  it('is an function',()=>{
+    expect(typeof dataLovers.pokeFilter).toBe('function');
+  });
+  it('debería retornar un arreglo al filtrar por tipo Grass', () => {
+    expect(Array.isArray(dataLovers.pokeFilter(pokemon.pokemon,"Grass"))).toBe(true); 
+  });
+});
+
